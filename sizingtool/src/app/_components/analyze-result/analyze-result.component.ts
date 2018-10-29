c03603b95d547bff0939f547e25778ba46df1aa4
mmll <632150409@qq.com>
2018-10-29 22:29:36 +0800

    update echart

--- sizingtool/src/app/_components/analyze-result/analyze-result.component.ts
+++ sizingtool/src/app/_components/analyze-result/analyze-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+const echarts = require('echarts');
 
 @Component({
   selector: 'app-analyze-result',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analyze-result.component.css']
 })
 export class AnalyzeResultComponent implements OnInit {
-
-  constructor() { }
+  myChart;
+  constructor() {
+  }
 
   ngOnInit() {
+    this.myChart = echarts.init(document.getElementById('main'));
+    this.myChart.setOption({
+      backgroundColor: '#2c343c',
+      visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+          colorLightness: [0, 1]
+        }
+      },
+      series : [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          data:[
+            {value:235, name:'视频广告'},
+            {value:274, name:'联盟广告'},
+            {value:310, name:'邮件营销'},
+            {value:335, name:'直接访问'},
+            {value:400, name:'搜索引擎'}
+          ],
+          roseType: 'angle',
+          label: {
+            normal: {
+              textStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              lineStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              }
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#c23531',
+              shadowBlur: 200,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    });
   }
 
 }
